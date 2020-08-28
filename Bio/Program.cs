e64c88eaf37803d647894ad32f2e1615b8f517c1
Fatma Alhouthi <fa222nx@student.lnu.se>
2020-08-28 14:15:15 +0200

    klart med meny 2

--- Bio/Program.cs
+++ Bio/Program.cs
@@ -7,7 +7,7 @@ namespace Bio
         static void Main(string[] args)
         {
             Console.WriteLine("Welcom to the main menu!");
-            Console.WriteLine("choose 1 to continue or 0 to close!");
+            Console.WriteLine("choose 1 to continue , choose 2 to type a sentence 10 times or 0 to close!");
             CustomerChoose();
         }
 
@@ -28,7 +28,13 @@ namespace Bio
                     ticket.TicketPris();
                     ticket.Demo();
                     Console.WriteLine($" The total price is   {ticket.TotalTicketsCost()}");
-                   
+                    break;
+
+                case 2:
+                    //Menyval 2: Upprepa tio gånger
+                    Console.WriteLine(" enter the sentence that you want to type 10 times");
+                    Repeat repeat = new Repeat();
+                    repeat.Iteraiton();
 
                     break;
                 default:
---
+++ Bio/Repeat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bio
+{
+    class Repeat
+    {
+        public string Input()
+        {
+            string input = Console.ReadLine();
+
+            return input;
+        }
+
+
+        public void Iteraiton()
+
+        {
+            string sentence = Input();
+            for (int i =1 ; i < 11; i++)
+            {
+                Console.Write($"{i}.{sentence}, ");
+            }
+        }
+    }
+}
